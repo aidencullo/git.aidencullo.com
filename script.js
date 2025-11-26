71a8cadfde066556c9b36b97e877deaad0f05e11
Aiden Cullo <aidencullo@gmail.com>
2025-11-25 19:19:05 -0500

    start anew

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-function appendTextElement(tagName, text, parent) {
-    const targetParent = parent || document.body;
-    const element = document.createElement(tagName);
-    element.textContent = text;
-    targetParent.appendChild(element);
-    return element;
-}
-
 const gh_events_endpoint = "https://api.github.com/users/aidencullo/events";
 
 fetch(gh_events_endpoint)
     .then(response => response.json())
     .then(events => {
-        const parent = document.body;
-        appendTextElement("h1", "Aiden Cullo", parent);
-        appendTextElement("h2", "GitHub", parent);
-        const filteredEvents = events.filter(event => event.created_at.includes("2025-10-10"));
-        appendTextElement("p", "Commits: " + filteredEvents.length);
-        })
+	console.log(events.length)
+	if (events) {
+	    console.log(events[0])
+	}
+    })
     .catch(error => {
         console.error("Error:", error);
     });
