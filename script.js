5a96ce8479121028f18fe65739e1c4ad67bc71ab
Aiden Cullo <aidencullo@gmail.com>
2025-11-25 19:32:12 -0500

    count events from today -- still errors

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ const gh_events_endpoint = "https://api.github.com/users/aidencullo/events";
 fetch(gh_events_endpoint)
     .then(response => response.json())
     .then(events => {
-	console.log(events.length)
-	if (events) {
-	    console.log(events[0])
+	let commits = 0
+	for (const event of events) {
+	    const timestamp = event.created_at
+	    const date = new Date(timestamp)
+	    const estDate = new Date(date.toLocaleString("en-US", { timeZone: "America/New_York" }));
+	    const now = new Date();
+	    const estNow = new Date(now.toLocaleString("en-US", { timeZone: "America/New_York" }));
+	    if (estNow.toDateString() === estDate.toDateString()){
+		commits++
+		console.log(event.repo.name)
+	    }
 	}
+	console.log(commits)
     })
     .catch(error => {
         console.error("Error:", error);
