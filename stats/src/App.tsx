fb7553c4ad1d295d79c502ecbad3fd743eb68a21
Aiden Cullo <aidencullo@gmail.com>
2025-12-02 15:31:11 -0500

    getting all repos, although there are duplicates

---
+++ stats/src/App.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+interface Repo {
+  id: number;
+  name: string;
+}
+
+function App() {
+  const [repos, setRepos] = useState<Repo[]>([]);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      const baseUrl = 'https://api.github.com/users/aidencullo/repos';
+      let page = 1;
+      let urlParams = new URLSearchParams({ per_page: '100', page: page.toString() });
+      let url = `${baseUrl}?${urlParams.toString()}`;
+      
+      while (true) {
+        const response = await fetch(url);
+        const data = await response.json();
+        setRepos(prev => [...prev, ...data]);
+        if (data.length === 0) {
+          break;
+        }
+        page++;
+        urlParams = new URLSearchParams({ per_page: '100', page: page.toString() });
+        url = `${baseUrl}?${urlParams.toString()}`;
+      }
+    };
+    fetchRepos();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My GitHub Repositories</h1>
+        {repos.length > 0 ? (
+          <ul>
+            {repos.map((repo) => (
+              <li key={repo.id}>{repo.name}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>Loading repositories...</p>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
